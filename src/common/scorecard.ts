f5450298b1df12dea7ba22ead03ae51641c62075
Manu <manu@hivecpq.com>
2020-02-11 10:30:14 +0100

    Various improvements - lines for cutting, cards are a bit smaller

--- src/common/scorecard.ts
+++ src/common/scorecard.ts
@@ -10,8 +10,7 @@ declare var pdfMake: any;
 })
 export class ScoreCardService {
 
-  constructor() {
-  }
+  private readonly SCORE_CARD_RESULT_WIDTH = 145;
 
   public printScoreCardsForAllFirstRoundsExceptFMC(wcif: Wcif) {
     let scorecards: ScoreCardInfo[] = [];
@@ -56,7 +55,7 @@ export class ScoreCardService {
       scorecards.push(scorecard);
     });
     if (roundNumber === 0) {
-      scorecards = this.sortScoreCards(scorecards);
+      scorecards = this.sortScoreCardsByName(scorecards);
     }
     this.addEmptyScoreCardsUntilPageIsFull(scorecards, wcif);
     this.print(wcif, scorecards);
@@ -132,6 +131,16 @@ export class ScoreCardService {
     }
   }
 
+  public printFourEmptyScorecards(wcif: Wcif) {
+    let scorecards: ScoreCardInfo[] = [
+      this.getEmptyScoreCard(wcif),
+      this.getEmptyScoreCard(wcif),
+      this.getEmptyScoreCard(wcif),
+      this.getEmptyScoreCard(wcif)
+    ];
+    pdfMake.createPdf(this.document(scorecards)).download('emptyScorecards-' + wcif.id + '.pdf');
+  }
+
   private getEmptyScoreCard(wcif): ScoreCardInfo {
     return {
       eventId: ' ',
@@ -171,26 +180,42 @@ export class ScoreCardService {
     for (let i = 0; i < scorecards.length; i += 4) {
       let onePage = [
         [
-          [this.getScoreCardTemplate(scorecards[i])],
-          '',
-          [this.getScoreCardTemplate(scorecards[i + 1])]
+          {stack: this.getScoreCardTemplate(scorecards[i]), border: [false, false, false, false]},
+          {text: '', border: [false, false, false, false]},
+          {text: '', border: [true, false, false, false]},
+          {stack: this.getScoreCardTemplate(scorecards[i + 1]), border: [false, false, false, false]}
         ],
         [
-          [this.getScoreCardTemplate(scorecards[i + 2])],
-          '',
-          [this.getScoreCardTemplate(scorecards[i + 3])]
+          {text: '', border: [false, true, false, false]},
+          {text: '', border: [false, true, false, false]},
+          {text: '', border: [true, true, false, false]},
+          {text: '', border: [false, true, false, false]}
+        ],
+        [
+          {stack: this.getScoreCardTemplate(scorecards[i + 2]), border: [false, false, false, false]},
+          {text: '', border: [false, false, false, false]},
+          {text: '', border: [true, false, false, false]},
+          {stack: this.getScoreCardTemplate(scorecards[i + 3]), border: [false, false, false, false]}
         ]
       ];
-      document.content.push({
+      let page = {
         table: {
-          heights: [385, 385],
-            widths: [265, 10, 265],
-            body: onePage
+          heights: [364, 22, 384],
+          widths: [260, 2, 8, 260],
+          body: onePage
         },
-        layout: 'noBorders',
+        layout: {
+          hLineColor: function (i, node) {
+            return '#d3d3d3';
+          },
+          vLineColor: function (i, node) {
+            return '#d3d3d3';
+          }
+        },
         margin: [-20,-10],
         pageBreak: 'after'
-      });
+      };
+      document.content.push(page);
     }
     document.content[document.content.length - 1].pageBreak = null;
     return document;
@@ -209,39 +234,37 @@ export class ScoreCardService {
     return [
       {text: info.competitionName, alignment: 'center', fontSize: 10},
       {text: info.eventName, alignment: 'center', fontSize: 18, bold: true},
-      {text: 'Round ' + (info.round === null ? '    ' : info.round)
-          + ' | Group ' + (info.group === null ? '    ' : info.group)
-          + ' of ' + (info.totalGroups === null ? '    ' : info.totalGroups)
-          + (info.ranking === null ? '' : ' | Place: ' + info.ranking), alignment: 'center', fontSize: 10},
+      (this.roundGroupAndPlaceInfo(info)),
       {table : {
-          widths: [30, 215],
+          widths: [30, this.SCORE_CARD_RESULT_WIDTH + 58],
           body: [[
             {text: (info.competitorId === null ? ' ' : info.competitorId), fontSize: 16, alignment: 'center'},
             {text: info.competitorName, fontSize: 16, alignment: 'center'}]]
         },margin: [0, 5]},
       {text: info.cumulative ? 'Also write down the time for a DNF!' : '', bold: true, alignment: 'center'},
       {table : {
-          widths: [5, 16, 157, 20, 20],
+          widths: [5, 16, this.SCORE_CARD_RESULT_WIDTH, 20, 20],
           body: [[
             {text:''},
             {text:'S', alignment: 'center'},
             {text:
                 info.cumulative ? 'Result\n(Cumulative limit: ' + info.timeLimit + ')' :
-                  (info.timeLimit !== null ? 'Result (DNF if ≥ ' + info.timeLimit + ')' : ''), alignment: 'center'},
+                  (info.timeLimit !== null ? 'Result (DNF if ≥ ' + info.timeLimit + ')' : ''),
+              alignment: 'center', fontSize: info.cumulative ? 10 : 12 },
             {text:'J', alignment: 'center'},
             {text:'C', alignment: 'center'}],
             [{text:'1', margin: [0, 7]}, '', '', '', '']]
         },margin: [0, 2]},
       {text: info.cutoff !== null ? '-------------- Continue if 1 < ' + info.cutoff +' --------------' : '', alignment: 'center', fontSize: 10},
       {table : {
-          widths: [5, 16, 157, 20, 20],
+          widths: [5, 16, this.SCORE_CARD_RESULT_WIDTH, 20, 20],
           body: [
             [{text:'2', margin: [0, 7]}, '', '', '', ''],
             [{text:'3', margin: [0, 7]}, '', '', '', '']]
         },margin: [0, 2]},
       {text: '-------------- Extra or provisional --------------', alignment: 'center', fontSize: 10},
       {table : {
-          widths: [5, 16, 157, 20, 20],
+          widths: [5, 16, this.SCORE_CARD_RESULT_WIDTH, 20, 20],
           body: [[{text:'E', margin: [0, 5]}, '', '', '', '']]
         },margin: [0, 2]}
     ]
@@ -251,25 +274,23 @@ export class ScoreCardService {
     return [
       {text: info.competitionName, alignment: 'center', fontSize: 10},
       {text: info.eventName, alignment: 'center', fontSize: 18, bold: true},
-      {text: 'Round ' + (info.round === null ? '    ' : info.round)
-          + ' | Group ' + (info.group === null ? '    ' : info.group)
-          + ' of ' + (info.totalGroups === null ? '    ' : info.totalGroups)
-          + (info.ranking === null ? '' : ' | Place: ' + info.ranking), alignment: 'center', fontSize: 10},
+      (this.roundGroupAndPlaceInfo(info)),
       {table : {
-          widths: [30, 215],
+          widths: [30, this.SCORE_CARD_RESULT_WIDTH + 58],
           body: [[
             {text: (info.competitorId === null ? ' ' : info.competitorId), fontSize: 16, alignment: 'center'},
             {text: info.competitorName, fontSize: 16, alignment: 'center'}]]
         },margin: [0, 5]},
       {text: info.cumulative ? 'Also write down the time for a DNF!' : '', bold: true, alignment: 'center'},
       {table : {
-          widths: [5, 16, 157, 20, 20],
+          widths: [5, 16, this.SCORE_CARD_RESULT_WIDTH, 20, 20],
           body: [[
             {text:''},
             {text:'S', alignment: 'center'},
             {text:
                 info.cumulative ? 'Result\n(Cumulative limit: ' + info.timeLimit + ')' :
-                  (info.timeLimit !== null ? 'Result (DNF if ≥ ' + info.timeLimit + ')' : ''), alignment: 'center'},
+                  (info.timeLimit !== null ? 'Result (DNF if ≥ ' + info.timeLimit + ')' : ''),
+              alignment: 'center', fontSize: info.cumulative ? 10 : 12 },
             {text:'J', alignment: 'center'},
             {text:'C', alignment: 'center'}],
             [{text:'1', margin: [0, 7]}, '', '', '', ''],
@@ -277,7 +298,7 @@ export class ScoreCardService {
         },margin: [0, 2]},
       {text: info.cutoff !== null ? '-------------- Continue if 1 or 2 < ' + info.cutoff +' --------------' : '', alignment: 'center', fontSize: 10},
       {table : {
-          widths: [5, 16, 157, 20, 20],
+          widths: [5, 16, this.SCORE_CARD_RESULT_WIDTH, 20, 20],
           body: [
             [{text:'3', margin: [0, 7]}, '', '', '', ''],
             [{text:'4', margin: [0, 7]}, '', '', '', ''],
@@ -285,7 +306,7 @@ export class ScoreCardService {
         },margin: [0, 2]},
       {text: '-------------- Extra or provisional --------------', alignment: 'center', fontSize: 10},
       {table : {
-          widths: [5, 16, 157, 20, 20],
+          widths: [5, 16, this.SCORE_CARD_RESULT_WIDTH, 20, 20],
           body: [[{text:'E', margin: [0, 5]}, '', '', '', '']]
         },margin: [0, 2]}
     ]
@@ -295,19 +316,16 @@ export class ScoreCardService {
     return [
       {text: info.competitionName, alignment: 'center', fontSize: 10},
       {text: info.eventName, alignment: 'center', fontSize: 18, bold: true},
-      {text: 'Round ' + (info.round === null ? '    ' : info.round)
-          + ' | Group ' + (info.group === null ? '    ' : info.group)
-          + ' of ' + (info.totalGroups === null ? '    ' : info.totalGroups)
-          + (info.ranking === null ? '' : ' | Place: ' + info.ranking), alignment: 'center', fontSize: 10},
+      (this.roundGroupAndPlaceInfo(info)),
       {table : {
-          widths: [30, 215],
+          widths: [30, this.SCORE_CARD_RESULT_WIDTH + 58],
           body: [[
             {text: (info.competitorId === null ? ' ' : info.competitorId), fontSize: 16, alignment: 'center'},
             {text: info.competitorName, fontSize: 16, alignment: 'center'}]]
         },margin: [0, 5]},
       {text: 'Count and write down the number of cubes before the attempt starts', bold: true, alignment: 'center'},
       {table : {
-          widths: [5, 16, 157, 20, 20],
+          widths: [5, 16, this.SCORE_CARD_RESULT_WIDTH, 20, 20],
           body: [[
             {text:''},
             {text:'S', alignment: 'center'},
@@ -324,7 +342,16 @@ export class ScoreCardService {
     ]
   }
 
-  private sortScoreCards(scorecards: ScoreCardInfo[]) {
+  private roundGroupAndPlaceInfo(info: ScoreCardInfo) {
+    return {
+      text: 'Round ' + (info.round === null ? '    ' : info.round)
+        + ' | Group ' + (info.group === null ? '    ' : info.group)
+        + ' of ' + (info.totalGroups === null ? '    ' : info.totalGroups)
+        + (info.ranking === null ? '' : ' | Place: ' + info.ranking), alignment: 'center', fontSize: 10
+    };
+  }
+
+  private sortScoreCardsByName(scorecards: ScoreCardInfo[]) {
     return scorecards.sort(function(a, b) {
       var textA = a.competitorName.toUpperCase();
       var textB = b.competitorName.toUpperCase();
